225d3ea8d1a28fd4c97d3cbcc5d18b5acccc97b9
Chris Cooper <chriscooper1991@gmail.com>
2020-05-23 22:13:57 -0700

    include js generation

--- gen_proto_bindings.sh
+++ gen_proto_bindings.sh
@@ -44,4 +44,4 @@ PROJECT_PATH=`pwd`
 #     -I ${PROJECT_PATH}/proto \
 #     ${PROJECT_PATH}/proto/*.proto
 
-./bin/protoc --plugin=protoc-gen-grpc-web=${PROJECT_PATH}/bin/protoc-gen-grpc-web --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:${PROTO_DEST} -I ${PROJECT_PATH}/proto ${PROJECT_PATH}/proto/*.proto
+./bin/protoc --plugin=protoc-gen-grpc-web=${PROJECT_PATH}/bin/protoc-gen-grpc-web --js_out=import_style=commonjs,binary:${PROTO_DEST} --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:${PROTO_DEST} -I ${PROJECT_PATH}/proto ${PROJECT_PATH}/proto/*.proto
