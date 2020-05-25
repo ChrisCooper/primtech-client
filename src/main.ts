e208801b13e40417553b763528fc7edbc5f7b214
Chris Cooper <chriscooper1991@gmail.com>
2020-05-24 19:49:10 -0700

    Fix node inclusion and add notes

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// process.version = 'v10.19.0';
+
 import Vue from 'vue';
 import * as grpcWeb from 'grpc-web';
 
@@ -7,8 +9,6 @@ import store from './store';
 import {Citizen, Void} from './proto/citizen_pb';
 import {CitizenServiceClient} from './proto/citizen_grpc_web_pb';
 
-import { credentials, ServiceError } from 'grpc';
-
 Vue.config.productionTip = false;
 
 new Vue({
@@ -17,29 +17,32 @@ new Vue({
 }).$mount('#app');
 
 
-const channelCredentials = credentials.createInsecure();
-const citizenClient = new CitizenServiceClient('http://localhost:8080');
+console.log("Starting client application");
+const citizenClient = new CitizenServiceClient('http://localhost:8980');
+console.log("Created citizen client");
 
 const clientCitizen = new Citizen();
 clientCitizen.setId(55);
 clientCitizen.setName("ClientyCitizen");
+console.log("Created client-side citizen");
 
 const request = new Void();
 
 const call = citizenClient.getCitizen(request, undefined,  (err: grpcWeb.Error, response: Citizen) => {
   if (err){
-    console.log(err.code);
-    console.log(err.message);
-    console.log(err);
+    console.log("Received error sending request", err);
     return;
   }
 
   console.log("Received citizen");
+  console.log(response);
   console.log(response.getId());
   console.log(response.getName());
 });
 
 call.on('status', (status: grpcWeb.Status) => {
+  console.log("Received new gRPC status");
+  console.log(status);
   console.log(status.code);
   console.log(status.details);
   console.log(status.metadata);
