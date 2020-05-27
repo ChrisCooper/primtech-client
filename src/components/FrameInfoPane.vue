a602c1691a9337dde653ef6efc992ba55cfc80d5
Chris Cooper <chriscooper1991@gmail.com>
2020-05-27 01:29:12 -0700

    Stream and display real frame info

--- src/components/FrameInfoPane.vue
+++ src/components/FrameInfoPane.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="frame_info">
     <h2>Frame Info</h2>
-    <span>Frame Number: {{ frameInfo.frameNumber }}</span>
+    <span>Frame Number: {{ frameInfo.getFrameNumber() }}</span>
+    <span>Actual time: {{ frameInfo.getActualFrameTimeNanos() }}</span>
+    <span>Target time: {{ frameInfo.getTargetFrameTimeNanos() }}</span>
+    <span>Percentage: {{ frameBudgetUsedPercent }}</span>
+    
     <div>
       <button v-on:click="pause">Pause</button>
     </div>
@@ -9,21 +13,40 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Vue, Provide } from 'vue-property-decorator';
+import { GameServiceClient } from '../proto/game_grpc_web_pb';
+import { Void } from '../proto/general_pb';
 import { FrameInfo } from '../proto/game_pb';
 
 @Component
 export default class FrameInfoPane extends Vue {
-  @Prop() private frameInfo: FrameInfo;
+  @Provide() private frameInfo: FrameInfo;
 
   constructor() {
     super();
     this.frameInfo = new FrameInfo();
     this.frameInfo.setFrameNumber(0);
+    this.frameInfo.setActualFrameTimeNanos(0);
+    this.frameInfo.setTargetFrameTimeNanos(0);
+  }
+
+  mounted(): void {
+    const gameClient = new GameServiceClient('http://localhost:8080');
+    const frameStream = gameClient.streamFrames(new Void(), undefined);
+    frameStream.on('data', this.onNewFrame);
+  }
+
+  onNewFrame(frameInfo: FrameInfo): void {
+    this.frameInfo = frameInfo;
   }
 
   pause(): void {
     console.log("Pausing");
+  }
+
+  get frameBudgetUsedPercent(): string {
+    const percentage = this.frameInfo.getActualFrameTimeNanos() * 1.0 / this.frameInfo.getTargetFrameTimeNanos() * 100
+    return percentage.toFixed(2) + "%"
   }
 }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import App from './App.vue';
 
 import {Citizen} from './proto/citizen_pb';
 import {CitizenServiceClient} from './proto/citizen_grpc_web_pb';
-import { GameServiceClient } from './proto/game_grpc_web_pb';
-import { FrameInfo } from './proto/game_pb';
 import { Void } from './proto/general_pb';
 
 Vue.config.productionTip = false;
@@ -15,40 +13,25 @@ new Vue({
   render: (h) => h(App),
 }).$mount('#app');
 
+function makeCitizenRequest() {
+  console.log("Starting client application");
+  const citizenClient = new CitizenServiceClient('http://localhost:8080');
 
-console.log("Starting client application");
-const citizenClient = new CitizenServiceClient('http://localhost:8080');
+  const request = new Void();
 
-const request = new Void();
+  const call = citizenClient.getCitizen(request, undefined,  (err: grpcWeb.Error, response: Citizen) => {
+    if (err){
+      console.log("Received error sending Citizen request", err);
+      return;
+    }
 
-const call = citizenClient.getCitizen(request, undefined,  (err: grpcWeb.Error, response: Citizen) => {
-  if (err){
-    console.log("Received error sending Citizen request", err);
-    return;
-  }
+    console.log("Received citizen");
+    console.log(response);
+  });
 
-  console.log("Received citizen");
-  console.log(response);
-  console.log(response.getId());
-  console.log(response.getName());
-});
+  call.on('status', (status: grpcWeb.Status) => {
+    console.log("Received new gRPC status");
+    console.log(status);
+  });
+}
 
-call.on('status', (status: grpcWeb.Status) => {
-  console.log("Received new gRPC status");
-  console.log(status);
-  console.log(status.code);
-  console.log(status.details);
-  console.log(status.metadata);
-});
-
-
-const gameClient = new GameServiceClient('http://localhost:8080');
-
-const frameStream = gameClient.streamFrames(request, undefined);
-
-frameStream.on('data', (frameInfo: FrameInfo) => {
-  console.log("New FrameInfo", frameInfo);
-});
-frameStream.on('status', function(status) {
-  console.log("FrameStream status: ", status);
-});
